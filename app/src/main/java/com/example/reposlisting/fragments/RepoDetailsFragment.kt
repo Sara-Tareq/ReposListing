de8fc2646d9631249114273e3de820b5fcb21bd3
Sara Ramadan <sara.tareq.ramadan@gmail.com>
2019-12-08 16:40:00 +0400

    adding empty details fragment

--- app/src/main/java/com/example/reposlisting/fragments/RepoDetailsFragment.kt
+++ app/src/main/java/com/example/reposlisting/fragments/RepoDetailsFragment.kt
@@ -1,6 +1,32 @@
 package com.example.reposlisting.fragments
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
+import com.example.reposlisting.view_models.RepoViewModel
 
 class RepoDetailsFragment: Fragment() {
+
+    private val viewModel: RepoViewModel by viewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel.repo.observe(this, Observer {
+
+        })
+
+
+    }
 }
--- app/src/main/java/com/example/reposlisting/view_models/RepoViewModel.kt
+++ app/src/main/java/com/example/reposlisting/view_models/RepoViewModel.kt
@@ -1,6 +1,10 @@
 package com.example.reposlisting.view_models
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import com.example.reposlisting.data.Repo
 
-class RepoViewModel :  ViewModel(){
+class RepoViewModel() : ViewModel() {
+
+    val repo: LiveData<Repo> = TODO()
 }
