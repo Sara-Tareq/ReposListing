6b422a8abb8016c2bfafd18ed6f6d771873441be
Sara Ramadan <sara.tareq.ramadan@gmail.com>
2019-12-11 10:30:54 +0400

    checking on the bookmarked index if exists in the updated list first

--- app/src/main/java/com/example/reposlisting/data/RepoDao.kt
+++ app/src/main/java/com/example/reposlisting/data/RepoDao.kt
@@ -8,16 +8,16 @@ import java.util.*
 interface RepoDao {
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insert(repo: Repo): Long
+    suspend fun insert(repo: Repo)
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insertAll(repos:List<Repo>): List<Long>
+    suspend fun insertAll(repos: List<Repo>)
 
     @Query("SELECT * FROM repositories where id = :repoId")
-     fun loadRepo(repoId: Int): LiveData<Repo>
+    fun loadRepo(repoId: Int): LiveData<Repo>
 
     @Query("SELECT * FROM repositories")
-     fun loadAllRepos(): LiveData<List<Repo>>
+    fun loadAllRepos(): LiveData<List<Repo>>
 
     @Query("SELECT * FROM repositories where isBookmarked = 1")
     fun loadBookmarkedRepos(): List<Repo>
@@ -29,11 +29,13 @@ interface RepoDao {
     suspend fun insertOrUpdate(repoList: List<Repo>) {
 
         val userBookmarkedRepos: List<Repo> = loadBookmarkedRepos()
-        if(userBookmarkedRepos.isEmpty()){
+        if (userBookmarkedRepos.isEmpty()) {
             insertAll(repoList)
-        }else{
-            for (bookmarked in userBookmarkedRepos){
-                repoList[repoList.indexOf(Repo(bookmarked.id))].isBookmarked = true
+        } else {
+            for (bookmarked in userBookmarkedRepos) {
+                val index: Int = repoList.indexOf(Repo(bookmarked.id))
+                if (index != -1)
+                    repoList[index].isBookmarked = true
             }
             insertAll(repoList)
         }
